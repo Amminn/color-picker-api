66eff3021c92431f5d17969e2aecf37b1566e3e7
Amminn <vrb.nnn@gmail.com>
2022-04-05 18:52:54 +0100

    Added function to copy hex values + comments

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const button = document.getElementById('button')
 const colorPlaceholder = document.querySelectorAll('.color > div')
 const colorPlaceholderHex = document.querySelectorAll('.color > p')
 
-console.log(colorPlaceholder)
-
+/* get color scheme button */
 button.addEventListener('click', () => {
 
+    /* get color value without # */
     const colorValue = color.value.slice(1, 7)
     const selectValue = select.value
 
-    console.log(colorValue)
-    console.log(selectValue)
-
+    /* Getting the data from the API */
     fetch(`https://www.thecolorapi.com/scheme?hex=${colorValue}&mode=${selectValue}&count=5`)
         .then(resp => resp.json())
         .then(data => {
-            
+
             const colorArray = data.colors
+
+            /* Printing color values and Changing background-colors */
             colorArray.map((item, index) => {
                 colorPlaceholder[index].style.backgroundColor = item.hex.value
                 colorPlaceholderHex[index].textContent = item.hex.value
@@ -26,11 +26,10 @@ button.addEventListener('click', () => {
         })
 })
 
-
-// colorPlaceholder.addEventListener('click', (e) => {
-//     console.log(e.currentTarget.tagName)
-// })
-
-colorPlaceholder.forEach((element, e) => {
-    console.log(e.currentTarget.tagName)
-});
+/* Copy color value function */
+colorPlaceholderHex.forEach(element => {
+    element.addEventListener('click', () => {
+      document.execCommand('copy')
+      element.innerHTML += `<span class="copied">Copied</span>` 
+    })
+})
